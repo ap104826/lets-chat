d6913efc621f52e5540b04e34d91e717ffd1a069
Arpita Mehta <arpita.patel02@gmail.com>
2020-07-04 17:33:54 -0700

    Updates

---
+++ precache-manifest.9329f235b23de3c065a93ed61591eb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa91bd74ff0f2271290ed0de0aabb9e",
+    "url": "/lets-chat/index.html"
+  },
+  {
+    "revision": "93f5f4c8382f8064ca70",
+    "url": "/lets-chat/static/css/main.6cbdf181.chunk.css"
+  },
+  {
+    "revision": "abdaea55f3e83b8e6611",
+    "url": "/lets-chat/static/js/2.09268393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lets-chat/static/js/2.09268393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f5f4c8382f8064ca70",
+    "url": "/lets-chat/static/js/main.d1e6490e.chunk.js"
+  },
+  {
+    "revision": "ec67fa1efdf6a3d398e2",
+    "url": "/lets-chat/static/js/runtime-main.eaabe5be.js"
+  }
+]);
