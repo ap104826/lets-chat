3afbb030629db136d913f43cf2f44b652f973198
Arpita Mehta <arpita.patel02@gmail.com>
2020-07-18 16:53:21 -0700

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_ENDPOINT: process.env.NODE_ENV === 'production' ?
-        'https://stark-bayou-31425.herokuapp.com/' : 'http://localhost:8001',
+        'https://stark-bayou-31425.herokuapp.com' : 'http://localhost:8001',
     TOKEN_KEY: 'letschat-client-auth-token',
     USERNAME_KEY: 'letschat-username',
 }
